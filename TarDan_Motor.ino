5776e80eed3137ad752eda2077b8510a07048b06
Fonseka100 <tarcisio78@hotmail.com>
2015-12-02 19:18:30 -0200

    Update TarDan_Motor.ino

--- TarDan_Motor.ino
+++ TarDan_Motor.ino
@@ -5,16 +5,37 @@
 //Stepper stepper(STEPS, 8, 9, 10, 11);
 //Stepper stepper(STEPS, 8, 9, 11, 10);
 Stepper stepper(STEPS, 8, 11, 9, 10);
-
+int ledPin=4;
+int relayPin = 5;
 int previous = 0;
 int val = 0;
+int lastValue = 800;
 
 void setup() {
   // put your setup code here, to run once:
-  stepper.setSpeed(60);
+  stepper.setSpeed(5);
+  pinMode(ledPin,OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(ledPin,HIGH);
+  Serial.begin(9600);
+  digitalWrite(relayPin, HIGH);
+
 }
 
 void loop() {
-  stepper.step(48);
-  delay(1000);
+  int sensorValue = analogRead(3);
+  Serial.println(sensorValue);
+  stepper.step(1);
+  if((lastValue - sensorValue) > 20){
+    digitalWrite(relayPin, LOW);
+    delay(1000);
+  } else 
+      digitalWrite(relayPin, HIGH);
+      
+  lastValue = sensorValue;
+
+    
+  delay(10);
 }
+
+
