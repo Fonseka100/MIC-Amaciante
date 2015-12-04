f64b8b22c6c7d4be5d1ef93f7f3c02f912922322
Fonseka100 <tarcisio78@hotmail.com>
2015-12-04 15:08:36 -0200

    Update TarDan_Motor.ino

--- TarDan_Motor.ino
+++ TarDan_Motor.ino
@@ -1,4 +1,6 @@
 #include <Stepper.h>
+#include <Wire.h>
+#include "rgb_lcd.h" // usar proprio pc, bilioteca no face
 
 #define STEPS 48
 
@@ -7,35 +9,63 @@
 Stepper stepper(STEPS, 8, 11, 9, 10);
 int ledPin=4;
 int relayPin = 5;
-int previous = 0;
-int val = 0;
+int sensorLuz = 3;
 int lastValue = 800;
+int button = 2;
+int buzzerPin = 6;
+
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
 
 void setup() {
   // put your setup code here, to run once:
   stepper.setSpeed(5);
   pinMode(ledPin,OUTPUT);
   pinMode(relayPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  
   digitalWrite(ledPin,HIGH);
-  Serial.begin(9600);
   digitalWrite(relayPin, HIGH);
+  
+  pinMode(button, INPUT_PULLUP); // interrupção
+  attachInterrupt(0, interrupcao, FALLING);
+  
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  int sensorValue = analogRead(3);
+  int sensorValue = analogRead(sensorLuz);
   Serial.println(sensorValue);
   stepper.step(1);
-  if((lastValue - sensorValue) > 20){
-    digitalWrite(relayPin, LOW);
-    delay(1000);
-  } else 
-      digitalWrite(relayPin, HIGH);
-      
+  if((lastValue - sensorValue) > 20)
+    putAmaciante();
+  
   lastValue = sensorValue;
-
-    
+ 
   delay(10);
 }
 
 
+void  putAmaciante(){
+  lastvalue = 800;
+  digitalWrite(relayPin, LOW);
+  digitalWrite(buzzerPin, HIGH);  
+  lcd.print("Coloque amaciante");
+  delay(500);
+  digitalWrite(buzzerPin, LOW);  
+  while(1){
+    
+  }
+}
+
+void interrupcao() {
+  lcd.clear();
+  digitalWrite(relayPin, HIGH);
+}
